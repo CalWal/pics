4cfaf4a799bd20a4708f649ace6c849977f3f930
CalWal <callumtrwallace@gmail.com>
2019-12-03 18:16:28 +1000

    Updates

---
+++ precache-manifest.94878d6648ae42de839487b0e561f942.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab0d50249972378ce0695f07f0d47b7",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "048eda2d5143007a8967",
+    "url": "/pics/static/css/main.d48cec2f.chunk.css"
+  },
+  {
+    "revision": "7b9ebaad6d9760426e4b",
+    "url": "/pics/static/js/2.204745cf.chunk.js"
+  },
+  {
+    "revision": "048eda2d5143007a8967",
+    "url": "/pics/static/js/main.7627ce4a.chunk.js"
+  },
+  {
+    "revision": "a21bb03d14b45902c32c",
+    "url": "/pics/static/js/runtime-main.92f9bd29.js"
+  }
+]);
